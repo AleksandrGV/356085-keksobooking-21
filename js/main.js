59d24abdbc7069f05e68dd47011fd8b369749a69
Александр Гончаренко <g.aleks80@gmail.com>
2020-09-28 21:40:22 +0500

    Иправления

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
 // 1
 
-const offerTitle = ['Уютная квартира в центре', 'Просторная квартира', 'Квартира с красивым видом ио окна', 'Квартира с площадкой для детей', 'Теплая квартира', 'Квартира в тихом районе города', 'Квартира с развитой инфроструктурой', 'Квартира рядом с метро'];
-const offerAddress = ['600, 350', '600, 450', '600, 550', '600, 250', '600, 150', '500, 250', '400, 100', '300, 300'];
-const offerType = ['palace', 'flat', 'house', 'bungalow'];
-const offerCheckinCheckout = ['12:00', '13:00', '14:00'];
-const offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const offerPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const offerTitle = [`Уютная квартира в центре`, `Просторная квартира`, `Квартира с красивым видом ио окна`, `Квартира с площадкой для детей`, `Теплая квартира`, `Квартира в тихом районе города`, `Квартира с развитой инфроструктурой`, `Квартира рядом с метро`];
+const offerAddress = [`600`, `350`, `350`, `250`, `150`, `500`, `400`, `300`];
+const offerType = [`palace`, `flat`, `house`, `bungalow`];
+const offerCheckinCheckout = [`12:00`, `13:00`, `14:00`];
+const offerFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const offerPhotos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 
 const MOCK_BOOKING_DATA = 8;
 
@@ -22,7 +22,7 @@ const getRandomNumbers = function (min, max) {
 const getMockBookingData = function () {
   return {
     'autor': {
-      'avatar': 'img/avatars/user' + getRandomNumbers(1, 8) + 'png'
+      'avatar': `img/avatars/user ${getRandomNumbers(1, 8) / 10} png`
     },
     'offer': {
       'title': offerTitle[(getRandomNumbers(0, 7))],
@@ -34,7 +34,7 @@ const getMockBookingData = function () {
       'checkin': offerCheckinCheckout[(getRandomNumbers(0, 2))],
       'checkuot': offerCheckinCheckout[(getRandomNumbers(0, 2))],
       'features': Array(getRandomNumbers(1, 4)).fill(offerFeatures[getRandomNumbers(0, offerFeatures.length - 1)]),
-      'description': 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
+      'description': `Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.`,
       'photos': Array(getRandomNumbers(1, 4)).fill(offerPhotos[getRandomNumbers(0, offerPhotos.length - 1)])
     },
     'location': {
@@ -44,7 +44,7 @@ const getMockBookingData = function () {
   };
 };
 
-const array = Array(MOCK_BOOKING_DATA).fill(getMockBookingData());
+const mockPinsData = Array(MOCK_BOOKING_DATA).fill(getMockBookingData());
 
 // console.log(array);
 
@@ -59,32 +59,37 @@ map.classList.remove('map--faded');
 let pinTemplate = document.querySelector('#pin').
 content.querySelector('.map__pin');
 
-let getCloneElement = function (img, elem, style) {
+// let getCloneElement = function (img, elem, style) {
 
-  img = {
-    src: '{{author.avatar}}',
-    alt: '{{Метка объявления}}'
-  };
 
-  for (let i = 0; i < array.length; i++) {
-    elem = pinTemplate.cloneNode(true);
-    elem.children[0].textContent = i;
+for (let i = 0; i < offerTitle.length; i++) {
+  let clonElement = pinTemplate.cloneNode(true);
+  clonElement.children[0].textContent = i;
+  clonElement.setAttribute(`style`, `left: ${offerAddress[getRandomNumbers(0, 7)]}px; top: ${offerAddress[getRandomNumbers(0, 7)]}px`);
+  let clonImg = pinTemplate.querySelector('img');
+  console.log(clonImg);
+  //Не работает
+  clonImg.setAttribute(`src`, `img/avatars/user0${getRandomNumbers(1, 8)}.png`, `alt`, `${getRandomNumbers(0, 7)}`);
+console.log(clonElement);
+}
+// };
 
-//style не работают, что-то не правильно делаю?
-
-    style = ('left: {{offerAddress[(getRandomNumbers(0, 7))] + 50}}px; top: {{offerAddress[(getRandomNumbers(0, 7))] + 30}}px');
-    console.log(elem);
-  }
-};
-
-getCloneElement();
+// getCloneElement();
 
 // 4
 
-let newCloneElement = function (str) {
-  var template = document.createElement('map__pins');
-  template.innerHTML = str;
-  return template.content;
+mockPinsData.foEach = function (element) {
+  let mapPins = document.createElement('map__pins');
+  mapPins.textContent = element;
+  let templateElement = document.createDocumentFragment();
+  templateElement.appendChild(mapPins);
+
 };
 
-newCloneElement();
+// let newCloneElement = function (str) {
+//   var template = document.mockPinsData;
+//   template.appendChild = str;
+//   return template.content;
+// };
+
+// console.log(newCloneElement());
