e7685e1a8aa7fc13a9b1f41013feea49f17f86c8
Александр Гончаренко <g.aleks80@gmail.com>
2020-10-04 13:08:46 +0500

    module3-task2 исправлены все ошибки

--- js/main.js
+++ js/main.js
@@ -4,17 +4,24 @@
 
 const MOCK_BOOKING_DATA = 8;
 
-const offerTitle = [`Уютная квартира в центре`, `Просторная квартира`, `Квартира с красивым видом из окна`, `Квартира с площадкой для детей`, `Теплая квартира`, `Квартира в тихом районе города`, `Квартира с развитой инфроструктурой`, `Квартира рядом с метро`];
-// const offerAddress = [`600`, `350`, `350`, `250`, `150`, `500`, `400`, `300`];
+const offerTitle = [`Уютная квартира в центре`, `Просторная квартира`,
+  `Квартира с красивым видом из окна`, `Квартира с площадкой для детей`,
+  `Теплая квартира`, `Квартира в тихом районе города`,
+  `Квартира с развитой инфроструктурой`, `Квартира рядом с метро`];
 const offerType = [`palace`, `flat`, `house`, `bungalow`];
 const offerCheckinCheckout = [`12:00`, `13:00`, `14:00`];
 const offerFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const offerPhotos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const offerPhotos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 
 
 const map = document.querySelector(`.map`);
 const mapPins = document.querySelector(`.map__pins`);
+
+// Template только для образца их нужно клонировать и взаимодействовать только с клонами
+
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
 
 // Функция получение случайного целого числа в заданном интервале, включительно
 
@@ -23,6 +30,28 @@ const getRandomNumbers = function (min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+// Функция создания и заполнения массива
+
+const getCreatePins = function () {
+  const arrPins = [];
+  for (let i = 0; i < MOCK_BOOKING_DATA; i++) {
+    arrPins.push(getMockBookingData());
+  }
+  return arrPins;
+};
+
+// Функция создания массива для фото карточки
+
+const getRandomPhotos = function () {
+  const photos = [];
+  for (let i = 0; i < getRandomNumbers(1, 3); i++) {
+    photos.push(offerPhotos[(getRandomNumbers(0, 2))]);
+  }
+  return photos;
+};
+
+// Функция для создания массива из сгенерированных объектов
 
 const getMockBookingData = function () {
   const markerX = getRandomNumbers(0, 1400);
@@ -44,8 +73,7 @@ const getMockBookingData = function () {
       'description': `Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
                       Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
                       Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.`,
-      'photos': offerPhotos[(getRandomNumbers(0, 2))]
-      // Array(getRandomNumbers(0, 2)).fill(offerPhotos[getRandomNumbers(0, offerPhotos.length - 1)])
+      'photos': getRandomPhotos()
     },
     'location': {
       'x': markerX,
@@ -54,19 +82,9 @@ const getMockBookingData = function () {
   };
 };
 
-// Функция создания и заполнения массива
-
-const getCreatePins = function () {
-  const arrPins = [];
-  for (let i = 0; i < MOCK_BOOKING_DATA; i++) {
-    arrPins.push(getMockBookingData());
-  }
-  return arrPins;
-};
-
 // Функция отрисовки клонированных элементов
 
-const getRenderingPins = function (pinsClone) {
+const renderingPins = function (pinsClone) {
   const templateElement = document.createDocumentFragment();
 
   pinsClone.forEach(function (pinNew) {
@@ -83,63 +101,55 @@ const getRenderingPins = function (pinsClone) {
 
 const mockPinsData = getCreatePins();
 
-getRenderingPins(mockPinsData);
+renderingPins(mockPinsData);
 
 map.classList.remove(`map--faded`);
 
-// Функция отрисовки карточки module3-task2
+// Функция отрисовки и клонирования фото
 
-const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+const getCreateHomePhoto = function (containerPhotoCard, photosCard) {
+  const fragmentPhotosCard = document.createDocumentFragment();
+
+  photosCard.forEach(function (photoCard) {
+    const templatePopupPhoto = containerPhotoCard.querySelector(`.popup__photo`).cloneNode(true);
+    templatePopupPhoto.setAttribute(`src`, photoCard);
+    fragmentPhotosCard.appendChild(templatePopupPhoto);
+  });
+  return fragmentPhotosCard;
+};
+
+// Функция отрисовки карточки объявления
 
 const createCard = function (cardNew) {
+
   const templateElementCard = document.createDocumentFragment();
 
-  const mapFilterContainer = document.querySelector(`.map__filter-container`);
+  const mapFilterContainer = document.querySelector(`.map__filters-container`);
   const clonCardTemplate = cardTemplate.cloneNode(true);
 
   // Короткая запись
 
-  cardTemplate.querySelector(`.popup__title`).textContent = cardNew.offer.title;
-  cardTemplate.querySelector(`.popup__text--address`).textContent = `${cardNew.offer.address}`;
-  cardTemplate.querySelector(`.popup__text--price`).textContent = `${cardNew.offer.price}₽/ночь`;
-  cardTemplate.querySelector(`.popup__type`).textContent = `${cardNew.offer.type}`;
-  cardTemplate.querySelector(`.popup__text--capacity`).textContent = (`${cardNew.offer.rooms} комнат(ы) для ${cardNew.offer.quests} гостей`);
-  cardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${cardNew.offer.checkin}, выезд до ${cardNew.offer.checkuot}`;
-  cardTemplate.querySelector(`.popup__features`).setAttribute(`li`, `${cardNew.offer.features}`);
-  cardTemplate.querySelector(`.popup__description`).textContent = `${cardNew.offer.description}`;
-  // cardTemplate.querySelector(`.popup__photos`).setAttribute(`src`, `${cardNew.offer.photos}`);
+  clonCardTemplate.querySelector(`.popup__avatar`).setAttribute(`src`, `${cardNew.autor.avatar}`);
+  clonCardTemplate.querySelector(`.popup__title`).textContent = cardNew.offer.title;
+  clonCardTemplate.querySelector(`.popup__text--address`).textContent = `${cardNew.offer.address}`;
+  clonCardTemplate.querySelector(`.popup__text--price`).textContent = `${cardNew.offer.price}₽/ночь`;
+  clonCardTemplate.querySelector(`.popup__type`).textContent = `${cardNew.offer.type}`;
+  clonCardTemplate.querySelector(`.popup__text--capacity`).textContent = (`${cardNew.offer.rooms} комнат(ы) для ${cardNew.offer.quests} гостей`);
+  clonCardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${cardNew.offer.checkin}, выезд до ${cardNew.offer.checkuot}`;
+  clonCardTemplate.querySelector(`.popup__features`).setAttribute(`li`, `${cardNew.offer.features}`);
+  clonCardTemplate.querySelector(`.popup__description`).textContent = `${cardNew.offer.description}`;
+
+  const containerCardPhotos = clonCardTemplate.querySelector(`.popup__photos`);
+
+  const homePhotoCard = getCreateHomePhoto(containerCardPhotos, cardNew.offer.photos);
+
+  containerCardPhotos.replaceChild(homePhotoCard, containerCardPhotos.querySelector(`.popup__photo`));
 
   templateElementCard.appendChild(clonCardTemplate);
+
+  // Вставляю карточку перед mapFilterContaine
 
   map.insertBefore(templateElementCard, mapFilterContainer);
 };
 
 createCard(mockPinsData[0]);
-
-
-// Функция клонирования фото
-const popupPhotos = cardTemplate.querySelector(`.popup__photos`); // нахожу класс popup__photos
-const popupPhoto = cardTemplate.querySelector(`.popup__photo`); // класс popup__photo
-
-const cardPhotos = function () {
-  const templateElementCard = document.createDocumentFragment();
-  const photosCard = [];
-  // photosCard.forEach(function (photoCard) {
-  for (let i = 0; i < photosCard.length; i++) {
-    const clonePopupPhotos = popupPhoto.cloneNode(); // клонирую класс popup_photo
-    clonePopupPhotos.children[0].textContent = photosCard[i];
-    clonePopupPhotos.setAttribute(`src`, `${photosCard[i].offer.photos}`); // вствляю в класс popup__photo src и путь к фото
-    // popupPhotos.setAttribute(`img`, clonePopupPhotos);
-    // console.log(clonePopupPhotos);
-    // popupPhotos.appendChild(clonPopupPhotos);
-    // console.log(popupPhotos);
-    popupPhotos.appendChild(clonePopupPhotos); // добавляю в обертку с классом popup__photos картинку с адресом
-  }
-  // });
-  return templateElementCard.appendChild(popupPhotos);
-  // return popupPhotos;
-  // console.log(cardTemplate);
-};
-
-// console.log(cardPhotos());
-cardPhotos(); // вызываю функцию
