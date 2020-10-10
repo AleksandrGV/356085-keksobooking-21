1427aca46e347118477a40984dac52801d0aed2f
Александр Гончаренко <g.aleks80@gmail.com>
2020-10-10 20:35:14 +0500

    10. Личный проект: доверяй, но проверяй (часть 1), удаллил map.classList.remove; вне функции

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const adFormSubmit = document.querySelector(`.ad-form__submit`);
 // Template только для образца их нужно клонировать и взаимодействовать только с клонами
 
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-// const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
 
 // Функция получение случайного целого числа в заданном интервале, включительно
 
@@ -109,58 +109,56 @@ const renderingPins = function (pinsClone) {
 
 const mockPinsData = getCreatePins();
 
-// renderingPins(mockPinsData);
+renderingPins(mockPinsData);
 
-// map.classList.remove(`map--faded`);
+// Функция отрисовки и клонирования фото
 
-// // Функция отрисовки и клонирования фото
+const getCreateHomePhoto = function (containerPhotoCard, photosCard) {
+  const fragmentPhotosCard = document.createDocumentFragment();
 
-// const getCreateHomePhoto = function (containerPhotoCard, photosCard) {
-//   const fragmentPhotosCard = document.createDocumentFragment();
+  photosCard.forEach(function (photoCard) {
+    const templatePopupPhoto = containerPhotoCard.querySelector(`.popup__photo`).cloneNode(true);
+    templatePopupPhoto.setAttribute(`src`, photoCard);
+    fragmentPhotosCard.appendChild(templatePopupPhoto);
+  });
+  return fragmentPhotosCard;
+};
 
-//   photosCard.forEach(function (photoCard) {
-//     const templatePopupPhoto = containerPhotoCard.querySelector(`.popup__photo`).cloneNode(true);
-//     templatePopupPhoto.setAttribute(`src`, photoCard);
-//     fragmentPhotosCard.appendChild(templatePopupPhoto);
-//   });
-//   return fragmentPhotosCard;
-// };
+// Функция отрисовки карточки объявления
 
-// // Функция отрисовки карточки объявления
+const createCard = function (cardNew) {
 
-// const createCard = function (cardNew) {
+  const templateElementCard = document.createDocumentFragment();
 
-//   const templateElementCard = document.createDocumentFragment();
+  const mapFilterContainer = document.querySelector(`.map__filters-container`);
+  const clonCardTemplate = cardTemplate.cloneNode(true);
 
-//   const mapFilterContainer = document.querySelector(`.map__filters-container`);
-//   const clonCardTemplate = cardTemplate.cloneNode(true);
+  // Короткая запись
 
-//   // Короткая запись
+  clonCardTemplate.querySelector(`.popup__avatar`).setAttribute(`src`, `${cardNew.autor.avatar}`);
+  clonCardTemplate.querySelector(`.popup__title`).textContent = cardNew.offer.title;
+  clonCardTemplate.querySelector(`.popup__text--address`).textContent = `${cardNew.offer.address}`;
+  clonCardTemplate.querySelector(`.popup__text--price`).textContent = `${cardNew.offer.price}₽/ночь`;
+  clonCardTemplate.querySelector(`.popup__type`).textContent = `${cardNew.offer.type}`;
+  clonCardTemplate.querySelector(`.popup__text--capacity`).textContent = (`${cardNew.offer.rooms} комнат(ы) для ${cardNew.offer.quests} гостей`);
+  clonCardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${cardNew.offer.checkin}, выезд до ${cardNew.offer.checkuot}`;
+  clonCardTemplate.querySelector(`.popup__features`).setAttribute(`li`, `${cardNew.offer.features}`);
+  clonCardTemplate.querySelector(`.popup__description`).textContent = `${cardNew.offer.description}`;
 
-//   clonCardTemplate.querySelector(`.popup__avatar`).setAttribute(`src`, `${cardNew.autor.avatar}`);
-//   clonCardTemplate.querySelector(`.popup__title`).textContent = cardNew.offer.title;
-//   clonCardTemplate.querySelector(`.popup__text--address`).textContent = `${cardNew.offer.address}`;
-//   clonCardTemplate.querySelector(`.popup__text--price`).textContent = `${cardNew.offer.price}₽/ночь`;
-//   clonCardTemplate.querySelector(`.popup__type`).textContent = `${cardNew.offer.type}`;
-//   clonCardTemplate.querySelector(`.popup__text--capacity`).textContent = (`${cardNew.offer.rooms} комнат(ы) для ${cardNew.offer.quests} гостей`);
-//   clonCardTemplate.querySelector(`.popup__text--time`).textContent = `Заезд после ${cardNew.offer.checkin}, выезд до ${cardNew.offer.checkuot}`;
-//   clonCardTemplate.querySelector(`.popup__features`).setAttribute(`li`, `${cardNew.offer.features}`);
-//   clonCardTemplate.querySelector(`.popup__description`).textContent = `${cardNew.offer.description}`;
+  const containerCardPhotos = clonCardTemplate.querySelector(`.popup__photos`);
 
-//   const containerCardPhotos = clonCardTemplate.querySelector(`.popup__photos`);
+  const homePhotoCard = getCreateHomePhoto(containerCardPhotos, cardNew.offer.photos);
 
-//   const homePhotoCard = getCreateHomePhoto(containerCardPhotos, cardNew.offer.photos);
+  containerCardPhotos.replaceChild(homePhotoCard, containerCardPhotos.querySelector(`.popup__photo`));
 
-//   containerCardPhotos.replaceChild(homePhotoCard, containerCardPhotos.querySelector(`.popup__photo`));
+  templateElementCard.appendChild(clonCardTemplate);
 
-//   templateElementCard.appendChild(clonCardTemplate);
+  // Вставляю карточку перед mapFilterContaine
 
-//   // Вставляю карточку перед mapFilterContaine
+  map.insertBefore(templateElementCard, mapFilterContainer);
+};
 
-//   map.insertBefore(templateElementCard, mapFilterContainer);
-// };
-
-// createCard(mockPinsData[0]);
+createCard(mockPinsData[0]);
 
 // Функция активации похожих объявлений
 
@@ -199,7 +197,6 @@ const unlocksFormFields = function () {
 
 const writeDownAddress = function (addressX, addressY) {
   inputAddress.value = (`${addressX}, ${addressY}`);
-  // console.log(inputAddress.value);
 };
 
 // Перевод страницы в активное состояние левой кнопкой мыши
@@ -207,7 +204,7 @@ const writeDownAddress = function (addressX, addressY) {
 mapPinMain.addEventListener(`mousedown`, function (evt) {
   if (evt.button === 0) {
     unlocksFormFields();
-    writeDownAddress(`${evt.x}, ${evt.y}`);
+    writeDownAddress(evt.x, evt.y);
   }
 });
 
@@ -222,19 +219,6 @@ mapPinMain.addEventListener(`keydown`, function (evt) {
 
 // Валидация полей формы
 
-// Функция связи селектов.
-
-const linkSelect = function () {
-  roomNumber.onchange = function () {
-    capacity.selectedIndex = this.selectedIndex;
-  };
-  capacity.onchange = function () {
-    roomNumber.selectedIndex = this.selectedIndex;
-  };
-};
-
-linkSelect();
-
 const checksAdTitleConditions = function () {
   if (roomNumber.value > capacity.value) {
     roomNumber.setCustomValidity(`Комнат и спальных мест больше чем гостей`);
@@ -245,7 +229,6 @@ const checksAdTitleConditions = function () {
   } else {
     roomNumber.setCustomValidity(``);
   }
-  roomNumber.reportVadility();
 };
 
 // Обработчик событий для проверки валидации комнат и гостей
